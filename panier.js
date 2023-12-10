24ee8666d87f60f70a5dff6094e912e5e7f1639f
jfzara <zarajeanfabrice@gmail.com>
2023-12-10 02:41:36 -0500

    page panier fonctionnelle

--- panier.js
+++ panier.js
@@ -1,16 +1,13 @@
-// Fonction d'initialisation appelée après le chargement de la page
-window.onload = function () {
-  // Mettez à jour le panier lors du chargement de la page
-  updatePanier();
-};
+// Identifiant de l'élément HTML de la liste du panier
+var panierListeElement = document.getElementById('panier-liste');
+
+// Identifiant de l'élément HTML pour afficher le total
+var totalPrixElement = document.getElementById('total-prix');
 
 // Fonction pour mettre à jour l'affichage du panier
 function updatePanier() {
   // Récupérer la liste d'articles depuis le Local Storage
-  var panier = JSON.parse(localStorage.getItem('panier')) || [];
-
-  // Sélectionner l'élément de la liste du panier
-  var panierListeElement = document.getElementById('panier-liste');
+  var panier = JSON.parse(localStorage.getItem('cart')) || [];
 
   // Vider le contenu actuel de la liste du panier
   panierListeElement.innerHTML = '';
@@ -20,36 +17,43 @@ function updatePanier() {
 
   // Parcourir chaque article dans le panier
   panier.forEach(function (article) {
-    // Créer un élément de liste pour chaque article
-    var listItem = document.createElement('li');
-    listItem.textContent = article.nom + ' x ' + article.quantite + ' - ' + article.prixTotal + ' €';
+    // Assurez-vous que les propriétés nécessaires existent (nom, quantite, prix)
+    if (article.hasOwnProperty('nom') && article.hasOwnProperty('quantite') && article.hasOwnProperty('prix')) {
+      // Créer un élément de liste pour chaque article
+      var listItem = document.createElement('li');
+      listItem.textContent = article.nom + ' x ' + article.quantite + ' - ' + article.prix * article.quantite + ' €';
 
-    // Ajouter un bouton pour supprimer l'article
-    var deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Supprimer';
-    deleteButton.addEventListener('click', function () {
-      removeFromPanier(article);
-    });
+      // Ajouter un bouton pour supprimer l'article
+      var deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Supprimer';
+      deleteButton.addEventListener('click', function () {
+        removeFromPanier(article);
+      });
 
-    // Ajouter le bouton de suppression à l'élément de la liste
-    listItem.appendChild(deleteButton);
+      // Ajouter le bouton de suppression à l'élément de la liste
+      listItem.appendChild(deleteButton);
 
-    // Ajouter l'élément de la liste au panier
-    panierListeElement.appendChild(listItem);
+      // Ajouter l'élément de la liste au panier
+      panierListeElement.appendChild(listItem);
 
-    // Mettre à jour le total
-    total += article.prixTotal;
+      // Mettre à jour le total
+      total += article.prix * article.quantite;
+    } else {
+      console.error('Article mal formaté dans le panier:', article);
+    }
   });
 
   // Mettre à jour l'affichage du total
-  var totalPrixElement = document.getElementById('total-prix');
-  totalPrixElement.textContent = 'Total : ' + total + ' €';
+  totalPrixElement.textContent = 'Total : ' + total.toFixed(2) + ' €';
+
+  // Ajouter une console.log pour voir les articles dans la console
+  console.log(panier);
 }
 
 // Fonction pour supprimer un article du panier
 function removeFromPanier(article) {
   // Récupérer la liste d'articles depuis le Local Storage
-  var panier = JSON.parse(localStorage.getItem('panier')) || [];
+  var panier = JSON.parse(localStorage.getItem('cart')) || [];
 
   // Trouver l'index de l'article dans le panier
   var index = panier.findIndex(function (item) {
@@ -67,7 +71,7 @@ function removeFromPanier(article) {
     }
 
     // Mettre à jour le Local Storage
-    localStorage.setItem('panier', JSON.stringify(panier));
+    localStorage.setItem('cart', JSON.stringify(panier));
 
     // Mettre à jour l'affichage du panier
     updatePanier();
@@ -77,7 +81,7 @@ function removeFromPanier(article) {
 // Fonction pour valider l'achat (checkout)
 function validerAchat() {
   // Ajoutez ici la logique pour valider l'achat, par exemple, vider le panier
-  localStorage.removeItem('panier');
+  localStorage.removeItem('cart');
 
   // Mettre à jour l'affichage du panier
   updatePanier();
@@ -86,8 +90,15 @@ function validerAchat() {
 // Fonction pour vider le panier
 function viderPanier() {
   // Supprimez le panier du Local Storage
-  localStorage.removeItem('panier');
+  localStorage.removeItem('cart');
 
   // Mettez à jour l'affichage du panier
   updatePanier();
 }
+
+// Appel à updatePanier lors du chargement de la page
+window.onload = function () {
+  updatePanier();
+};
+
+
